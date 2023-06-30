1d82859233f263b20f380e898214822c0acab2c0
diegocgr <diegocgr@gmail.com>
2023-07-01 00:11:27 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 let getContainerPrincipal = document.getElementById("container-principal");
 getContainerPrincipal.style.display = "none";
 
+let peticionTexto = "";
 alert("Hola...antes de seguir te haré una pregunta...");
-let peticionTexto = prompt('¿Diminutivo cariñoso?').toLocaleLowerCase();
-
-// console.log("texto: " + peticionTexto);
 
 while(peticionTexto != "cosita") {
     let peticionTexto = prompt('¿Diminutivo cariñoso?').toLocaleLowerCase();
 
     if(peticionTexto == "cosita") {
-        alert("SIIIIII");
+        alert("¡Chachi!");
         getContainerPrincipal.style.display = "block";
         break;
     }else{
-        alert("NUUUUUU");
+        alert("Nuuuuuu");
 
     }
 };
